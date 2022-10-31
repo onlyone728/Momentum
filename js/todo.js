0b18bfdd39148ca7d344c5b8ce5085613c532b54
wonny <onlyone728@gmail.com>
2022-10-31 18:52:58 +0900

    위치 정보로 날씨 추가

--- js/todo.js
+++ js/todo.js
@@ -9,6 +9,8 @@ let toDos = [];
 function deleteToDo(e) {
      const li = e.target.parentElement;
      li.remove();
+     toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+     saveToDos(toDos);
 }
 
 function saveToDos() {
@@ -17,8 +19,9 @@ function saveToDos() {
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
@@ -31,8 +34,12 @@ function handleToDoSubmit(e) {
     e.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
@@ -48,5 +55,4 @@ if(savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
     toDos = parsedToDos;
     parsedToDos.forEach(paintToDo);
-}
-
+} 
